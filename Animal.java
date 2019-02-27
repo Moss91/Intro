ab3afad1871b734f6e423724f4e91642899e1fd4
Moss91 <47772123+Moss91@users.noreply.github.com>
2019-02-27 14:40:11 +0200

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter8.Animal;
+
+/**
+ *
+ * @author TiehoM
+ */
+public class Animal {
+    public void makeNoise(){
+         System.out.println("Super makes noise");
+     }
+    public void eat(){
+         System.out.println("Super eat()");
+     }
+    public void sleep(){
+        System.out.println("Super sleep()");
+    }
+    public void roam(){
+        System.out.println("Super roam()");
+    }
+}
